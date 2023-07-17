4b2e4796bd06421a8c3d81ff071979cf5dd2a55c
Torgovatel <kazanin.02@mail.ru>
2023-07-17 23:00:17 +0300

    Fix sql scripts for preview-column.

--- website/application/database/initialization/PostgreSQL/content_columns.sql
+++ website/application/database/initialization/PostgreSQL/content_columns.sql
@@ -1,3 +1,4 @@
 ALTER TABLE IF EXISTS public.bird_species
     ADD video CHARACTER VARYING(30) NULL,
+    ADD preview CHARACTER VARYING(30) NULL DEFAULT 'undefined.png',
     ADD avatar CHARACTER VARYING(30) NULL DEFAULT 'undefined.jpg';
